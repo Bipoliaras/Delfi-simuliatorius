0bec40594993eafe6b78a48e504fcca55bb5367c
Ernestas <wolfas560@gmail.com>
2019-07-14 21:22:12 +0300

    Refactoring

--- Scraper/src/main/java/Main.java
+++ Scraper/src/main/java/Main.java
@@ -2,46 +2,65 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.bson.Document;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
-  //btnLoadmore
-  public static String websiteLink = "https://www.delfi.lt/news/daily/lithuania/feisbuke-plinta-nuotrauka-is-sventosios-parduotuves-atostogauja-taip-kad-praranda-nuovoka.d?id=81606817&com=1";
+  private static Database db;
 
+  public static final String websiteLink = "https://www.delfi.lt/";
 
   public static void main(String args[]) {
 
-    MongoClient mongoClient = new MongoClient();
-    MongoDatabase localDatabase = mongoClient.getDatabase("local");
-    MongoCollection<Document> commentCollection = localDatabase.getCollection("Comments");
+    db = new Database();
+    db.init();
+
     System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
 
     WebDriver driver = new ChromeDriver();
+    driver.get(websiteLink);
 
-    driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    List<String> links = ((ChromeDriver) driver).findElementsByTagName("a").stream()
+        .map(link -> link.getAttribute("href"))
+        .filter(link -> link != null && link.contains("&com=1"))
+        .collect(Collectors.toList());
 
+    links.forEach(link -> scrapeComments(link, driver));
 
-    ((ChromeDriver) driver).findElementByClassName("select-type-text").click();
+    driver.close();
+  }
 
-    WebElement loadMoreCommentsButton = ((ChromeDriver) driver).findElementById("btnLoadmore");
+  private static void scrapeComments(String link, WebDriver driver) {
 
-    while (loadMoreCommentsButton != null) {
-      loadMoreCommentsButton.click();
+    driver.get(link);
 
-      loadMoreCommentsButton = ((ChromeDriver) driver).findElementById("btnLoadmore");
-    }
+   // ((ChromeDriver) driver).findElementByClassName("select-type-text").click();
 
-    List<WebElement> comments = ((ChromeDriver) driver).findElementsByClassName("comment-content");
+//    WebElement loadMoreCommentsButton = ((ChromeDriver) driver).findElementById("btnLoadmore");
+//
+//    while (loadMoreCommentsButton != null) {
+//      loadMoreCommentsButton.click();
+//
+//      loadMoreCommentsButton = ((ChromeDriver) driver).findElementById("btnLoadmore");
+//    }
 
-    for (WebElement comment : comments) {
-      commentCollection.insertOne(new Document("comment", comment.getText()));
-    }
+    List<String> comments = ((ChromeDriver) driver).findElementsByClassName("comment-content")
+          .stream()
+          .map(comment -> comment.getText())
+          .collect(Collectors.toList());
+
+    comments.forEach(comment -> db.saveComment(comment));
 
   }
+
 }
