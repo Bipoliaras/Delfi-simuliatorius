0c22b30949f1cd0b4288e517d6a370e0a9b55ccd
unknown <aivaras.goncarovas@gmail.com>
2020-02-21 17:28:10 +0200

    deleted:    data/example.json 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	deleted:    src/App.js 	new file:   src/components/GridFigure.js 	new file:   src/components/GridFigureStyles.js 	new file:   src/components/NewsGrid.js 	modified:   src/components/NotFound.js 	modified:   src/components/Router.js 	new file:   src/data/B2B.js 	renamed:    data/example.js -> src/data/example.js 	modified:   src/index.css 	modified:   src/index.js

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import styled, { keyframes } from 'styled-components';
 
-const NotFound = () => (
-  <header className="top">
-    <div>Not Found!!!!</div>
-  </header>
-);
+const AppLogoSpin = (from, to) => keyframes`
+  from {
+    transform: rotate(${from}deg);
+  }
+  to {
+    transform: rotate(${to}deg);
+  }
+`;
+
+const StyledNotFound = styled.div`
+  text-align: center;
+  .logo {
+    @media (prefers-reduced-motion: no-preference) {
+      animation: ${AppLogoSpin(0, 360)} infinite 20s linear;
+    }
+    height: 40vmin;
+    pointer-events: none;
+  }
+  .header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+  .link {
+    color: #61dafb;
+  }
+`;
+
+function NotFound() {
+  return (
+    <StyledNotFound>
+      <header className='header'>
+        <img src={logo} className='logo' alt='logo' />
+        Path: {window.location.href} not found!
+        <span className='link'>
+          <Link to='/'>Go Home Brother</Link>
+        </span>
+      </header>
+    </StyledNotFound>
+  );
+}
 
 export default NotFound;
---
+++ client/src/data/B2B.js
@@ -0,0 +1,48 @@
+const trash = {
+  'ef673bf1-682f-4bfa-b590-ccbe03f103f7': {
+    createdOn: '2015-03-25T12:00:00Z',
+    title: 'Turn around 5 times to feel better',
+    headline: 'Spinned 15 times for greater effect, felt worse :puke',
+    display_src: `https://picsum.photos/400/400/?image=${Math.floor(
+      Math.random() * 85
+    )}`,
+    comments: [
+      {
+        user: 'Me',
+        text: 'Totally need to try this.'
+      },
+      {
+        user: 'Danny',
+        text: 'Dan. WE should have lunch.'
+      }
+    ]
+  },
+  'ef673bf1-682f-4bfa-b590-ccbe03f103f6': {
+    createdOn: '2015-03-25T12:00:00Z',
+    title: 'Turn around 5 times to feel better',
+    headline: 'Spinned 15 times for greater effect, felt worse :puke',
+    display_src: `https://picsum.photos/400/400/?image=${Math.floor(
+      Math.random() * 85
+    )}`,
+    comments: [
+      {
+        user: 'Me',
+        text: 'Totally need to try this.'
+      },
+      {
+        user: 'Danny',
+        text: 'Dan. WE should have lunch.'
+      },
+      {
+        user: 'Danny',
+        text: 'Dan. WE should have lunch.'
+      },
+      {
+        user: 'Danny',
+        text: 'Dan. WE should have lunch.'
+      }
+    ]
+  }
+};
+
+export default trash;
