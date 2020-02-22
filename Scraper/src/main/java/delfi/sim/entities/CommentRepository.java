e260e83a1bd70dee661c9d91179e3bcb392807da
Bipoliaras <wolfas560@gmail.com>
2020-02-22 13:57:11 +0200

    Add random queries to repositories

--- Scraper/src/main/java/delfi/sim/entities/CommentRepository.java
+++ Scraper/src/main/java/delfi/sim/entities/CommentRepository.java
@@ -1,7 +1,14 @@
 package delfi.sim.entities;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> { }
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  @Query(value = "SELECT * FROM t_comment ORDER BY random() LIMIT ?1", nativeQuery = true)
+  List<Comment> findRandomComments(Integer limit);
+
+}
--- Scraper/src/main/java/delfi/sim/entities/HeadlineRepository.java
+++ Scraper/src/main/java/delfi/sim/entities/HeadlineRepository.java
@@ -1,8 +1,14 @@
 package delfi.sim.entities;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HeadlineRepository extends JpaRepository<Headline, Long> {
+
+  @Query(value = "SELECT * FROM t_headline ORDER BY random() LIMIT 1", nativeQuery = true)
+  Headline findRandomHeadline();
+
 }
