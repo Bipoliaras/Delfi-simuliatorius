be665f6f13adf343fe9a563ae5a67f076cca65d9
Bipoliaras <wolfas560@gmail.com>
2020-03-01 17:06:28 +0200

    Refactor

---
+++ Scraper/src/main/java/delfi/sim/scraper/WebsiteLinks.java
@@ -0,0 +1,20 @@
+package delfi.sim.scraper;
+
+public enum WebsiteLinks {
+  NEWS("https://www.delfi.lt/news/daily/lithuania/"),
+  CRIME("https://www.delfi.lt/news/daily/crime/"),
+  WORLD("https://www.delfi.lt/news/daily/world/"),
+  LIFE("https://www.delfi.lt/gyvenimas/"),
+  SCIENCE("https://www.delfi.lt/mokslas/");
+
+  private String url;
+
+  WebsiteLinks(String url) {
+    this.url = url;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+}
